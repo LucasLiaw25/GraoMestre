2abf5aeaea145f0a4d5d254067bbe998b135f38e
LucasLiaw25 <lucasaliaw@gmail.com>
2026-03-07 18:09:41 -0300

    feat: Create handle bad credentials methods

--- src/main/java/com/liaw/dev/GraoMestre/exception/HandleException.java
+++ src/main/java/com/liaw/dev/GraoMestre/exception/HandleException.java
@@ -3,6 +3,7 @@ package com.liaw.dev.GraoMestre.exception;
 import com.liaw.dev.GraoMestre.exception.exceptions.ConflitException;
 import com.liaw.dev.GraoMestre.exception.exceptions.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,6 +26,16 @@ public class HandleException {
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(ConflitException.class)
     public ErrorMessage handleConflitException(ConflitException e){
+        return new ErrorMessage(
+                HttpStatus.CONFLICT.value(),
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(BadCredentialsException.class)
+    public ErrorMessage handleBadCredentialsException(BadCredentialsException e){
         return new ErrorMessage(
                 HttpStatus.CONFLICT.value(),
                 e.getMessage(),
