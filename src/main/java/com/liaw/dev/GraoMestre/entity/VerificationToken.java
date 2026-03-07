04e32e640ee29fbcc9d938bbf330e5664632036b
LucasLiaw25 <lucasaliaw@gmail.com>
2026-03-07 17:37:32 -0300

    feat: Create verification token

--- src/main/java/com/liaw/dev/GraoMestre/entity/VerificationToken.java
+++ src/main/java/com/liaw/dev/GraoMestre/entity/VerificationToken.java
@@ -1,4 +1,38 @@
 package com.liaw.dev.GraoMestre.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "tb_verification_token")
 public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    public VerificationToken(String token, User user, LocalDateTime expiryDate) {
+        this.token = token;
+        this.user = user;
+        this.expiryDate = expiryDate;
+    }
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expiryDate);
+    }
 }
--- src/main/java/com/liaw/dev/GraoMestre/repository/VerificationTokenRepository.java
+++ src/main/java/com/liaw/dev/GraoMestre/repository/VerificationTokenRepository.java
@@ -1,4 +1,7 @@
 package com.liaw.dev.GraoMestre.repository;
 
-public interface VerificationTokenRepository {
+import com.liaw.dev.GraoMestre.entity.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 }
